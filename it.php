e619d655261cbc640b7f4f3aef436e83982c8dd4
Dhiru Sarania <dhirusarania@users.noreply.github.com>
2020-02-18 17:33:49 +0530

    sdsdsd

--- it.php
+++ it.php
@@ -6,41 +6,67 @@ $data = array();
 
 
 $data = array(
-    
+
+    //login
+    'appointments' => "Appointments",
+    'username' => "nome utente",
+    'password' => "password",
+    'change_password' => "Cambia password",
+
+
+    "filters" => "Filtri",
+
+    "appointment_state" => "Mostra",
+
+    "color_appointment" => "Colori appuntamenti",
+
+    "customer_name" => 'Cliente',
+    "customer_email" => 'Customer Email',
+    "customer_phone" => 'Customer Phone',
+
+
+    "service" => "Servizio",
+    "employee" => "Staff",
+    "available_appointment_time" => "Orari disponibili",
+    "no_free_slots" => "Nessun orario disponibili",
+    "close" => "Chiudi",
+    "save" => "Save",
+    "create_appointment" => "Salva",
+    "delete" => "Elimina",
+    "update_appointment" => "Salva",
+    "selected_appointment" => "Salva",
+
+
+    //personal appointment
+    'personal_appointments' => 'Appuntamento personale',
+    'employees' => 'Staff',
+    'appointment_type' => 'Tipo appuntamento',
+    'description' => 'Descrizione',
+    'duraration' => 'Durata  in minuti',
+    'date' => 'Data',
+    'save_changes' => 'Salva',
+
+
     //menu items
     'appointments' => "Appointments",
-    'create_appointments' => "Create Appointments",
-    'create_personal_appointments' => "Personal Appointments",
+    'create_appointments' => "Nuovo appuntamento",
+    'create_personal_appointments' => "Appuntamento personale",
     'overview' => "Overview",
     'shortcuts' => "Shortcuts",
-    'filters' => "Filters",
-    'appointment_state' => "Appointment State",
-    
+
     //dashboard
     'select_date' => "Select Date",
     'zoom_in' => "Zoom In",
     'zoom_out' => "Zoom Out",
-    
+
     //update Appointment Popup
-    'selected_appointment' => "Selected Appointment",
-    'customer_name' => "Customer Name",
-    'service' => "Service",
     'current_time' => "Current Time",
     'duration' => "Duration",
-    'date' => "Date",
-    
+
     //update personal Appointment Popup
-    'employees' => "Employees",
-    'appointment_type' => "Appointment Type",
-    'description' => "Description",
-    'duration' => "Duration",
-    'date' => "Date",
 
-
-    'delete' => 'Delete',
-    'close' => 'Close',
     'save_appointment' => 'Save Appointment'
-    
+
 );
 
 
@@ -48,5 +74,3 @@ $data = array(
 $res = json_encode($data);
 echo $res;
 exit;
-
-?>
